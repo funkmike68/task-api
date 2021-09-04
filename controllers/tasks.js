bdd63155dc0de784be932be85ce1812eb5355902
Michael Dodson <thinkwellpub@gmail.com>
2021-09-04 09:16:44 -0500

    Branch - 4
    Branch - 4. Updates for testing in Postman.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,11 +3,11 @@ const getAllTasks = (req, res) => {
 };
 
 const createTask = (req, res) => {
-  res.send("Create a task");
+  res.json(req.body);
 };
 
 const getTask = (req, res) => {
-  res.send("Get a Single Task");
+  res.json({ id: req.params.id });
 };
 const updateTask = (req, res) => {
   res.send("Update Task");
