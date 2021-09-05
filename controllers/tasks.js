ea35db6f83d1f146962c5ed0045e12220b105bb0
Michael Dodson <thinkwellpub@gmail.com>
2021-09-05 10:32:02 -0500

    Branch - 12
    Branch - 12. Completed the delete task controller.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -34,8 +34,18 @@ const getTask = async (req, res) => {
 const updateTask = (req, res) => {
   res.send("Update Task");
 };
-const deleteTask = (req, res) => {
-  res.send("Delete Task");
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
