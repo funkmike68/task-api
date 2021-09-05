0a2a51053a0d14723cb6b2d6fa88a6b0dfadff8e
Michael Dodson <thinkwellpub@gmail.com>
2021-09-05 09:17:09 -0500

    Branch - 10
    Branch - 10. Completing the get all tasks controller.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,11 @@
 const Task = require("../models/Task");
-const getAllTasks = (req, res) => {
-  res.send("Get all tasks.");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
