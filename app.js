b99c5a6445ce4eb96555f7465dbe80ee58a9ec3a
Michael Dodson <thinkwellpub@gmail.com>
2021-09-06 22:29:49 -0500

    Branch - 17
    Branch - 17. Added custom error handler.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error");
 
 // middleware
 app.use(express.static("./public"));
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/api/v1/tasks", tasks);
 
 app.use(notFound);
+app.use(errorHandler);
 
 const port = 3000;
 
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,7 @@
 const Task = require("../models/Task");
 const asyncWrapper = require("../middleware/async");
+const errorHandler = require("../middleware/error");
+const { createCustomError } = require("../errors/customerror");
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
@@ -11,11 +13,11 @@ const createTask = asyncWrapper(async (req, res) => {
   res.status(201).json({ task });
 });
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
 
   res.status(200).json({ task });
@@ -30,7 +32,7 @@ const updateTask = asyncWrapper(async (req, res) => {
   });
 
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
 
   res.status(200).json({ task });
@@ -40,7 +42,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
